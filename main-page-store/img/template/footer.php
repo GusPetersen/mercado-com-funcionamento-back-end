1391113d0440f999091e9ed532211b07a8af903b
Gustavo Vizzotto Petersen <144279568+GusPetersen@users.noreply.github.com>
2024-09-05 23:03:11 -0300

    Add files via upload

---
+++ main-page-store/img/template/footer.php
@@ -0,0 +1,59 @@
+<footer>
+    <section class="footerCP">
+        <ul class="footerC1">
+            
+            <li><h2 class="Ftitle">Conteúdo do site:</h2></li>
+            <li>
+                <p class="Flist"><a class="linkC" href="index.html">Home</a></p>
+                <p class="Flist"><a class="linkC" href="inscricao.html">Padaria</a></p>
+                <p class="Flist"><a class="linkC" href="galeria.html">Mercado</a></p>
+                <p class="Flist"><a class="linkC" href="conteudo.html#idsoft">Conta</a></p>
+                <p class="Flist"><a class="linkC" href="conteudo.html#idsobre">Sobre</a></p>
+                <p> <br> <hr></p>
+            </li>
+
+        </ul>
+        <ul class="footerC2">
+            <li>
+                <a class="linkC" href="https://calabria.com.br/">
+                <p id="siteC">GustoEmporio.com.br</p></a>
+            </li>
+            <li>
+                <img class="logoR" src="img/bobora.png"  alt="logoR"></a>
+            </li>
+        <p class="copyright"> &copy; copyright All rights reserved - 2023</p>
+        </ul>
+        <ul class="footerC3">
+           <div class="C3">
+                <li class="Ftitle">
+                    <p>Fale conosco:</p>
+                </li>
+                <li>
+                    <hr>
+                    <br>
+                    <p class="Fcontat">Número: (51) 98765-4321  </p>
+                    <p class="Fcontat">contato por email</p>
+                    <br>
+                    <hr>
+                </li>
+           </div> 
+           <div class="C3">
+                <li class="Ftitle">
+                    <h2>Mídias Sociais:</h2></li>
+                <li class="Fimgs">
+                    <a href="https://www.instagram.com/redecalabria/" target="_blank"><img class="FSimg" src="img/insta.png"></a>
+                    <a href="https://www.youtube.com/@blogdocalabria" target="_blank"><img class="FSimg" src="img/youtube.png"></a>
+                    <a href="https://www.facebook.com/redecalabria/?locale=pt_BR" target="_blank"><img class="FSimg" src="img/face.png"></a>
+                </li>
+                <li>
+                    <p class="Ftitle">Local: </p>
+                    <p>Estrada Aracaju, 650-Vila Nova,<br> Porto Alegre - RS, 91740-320</p>
+                    
+            </div>  
+            </li>
+        </ul>
+    </section>
+</footer>
+
+</body>
+</html>
